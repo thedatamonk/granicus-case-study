35132dce707e46aefd6154c39c6d0b18ce5ed219
Rohil Pal <rohilpal9763@gmail.com>
2026-01-08 09:10:23 +0530

    Remove hardcoded path

--- src/apis/docparser/src/docparser/settings.py
+++ src/apis/docparser/src/docparser/settings.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     project_name: str = "docparser"
@@ -7,11 +7,15 @@ class Settings(BaseSettings):
     openapi_url: str = "/openapi.json"
     api_prefix: str = ""
     allowed_hosts: list[str] = ["*"]
-    processed_docs_dir: str = "/Users/rohil/rohil-workspace/career/interviews/granicus/src/apis/docparser/.processed_data"
+    processed_docs_dir: str = ".processed_data"
     embedder_service_url: str = "http://localhost:8001"
     embedder_timeout: int = 300
     weaviate_url: str = "http://localhost:8080"
     weaviate_collection: str = "govdocs2"
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
     
 
 @lru_cache
