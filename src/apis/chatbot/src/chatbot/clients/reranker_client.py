ab8b5def06fca6c94be826e61f09eead57b02ab9
Rohil Pal <rohilpal9763@gmail.com>
2026-01-14 10:45:54 +0530

    Incorporate a smaller reranking model for faster inference

--- src/apis/chatbot/src/chatbot/clients/reranker_client.py
+++ src/apis/chatbot/src/chatbot/clients/reranker_client.py
@@ -2,11 +2,16 @@ from chatbot.settings import get_settings
 from loguru import logger
 from transformers import AutoModel
 from typing import Dict, Any, List
+from abc import ABC, abstractmethod
 
 settings = get_settings()
 
+class BaseRerankerClient(ABC):
+    @abstractmethod
+    def rerank(self, query: str, docs: List[Dict[str, Any]]):
+        pass
 
-class RerankerClient:
+class JinaLargeRerankerClient(BaseRerankerClient):
     def __init__(self) -> None:
         logger.info(f"Loading reranker model: {settings.reranker_model_name}")
         self.client = AutoModel.from_pretrained(
@@ -27,12 +32,36 @@ class RerankerClient:
         
         return final_results
 
+class JinaSmallRerankerClient(BaseRerankerClient):
+    def __init__(self) -> None:
+        logger.info(f"Loading reranker model: {settings.reranker_model_name}")
+        from sentence_transformers import CrossEncoder
+
+        self.client = CrossEncoder(settings.reranker_model_name, trust_remote_code=True)
+        logger.info("Loaded successfully!")
+
+    def rerank(self, query: str, docs: List[Dict[str, Any]]):
+        reranked_results = self.client.rank(query, 
+                                            documents=list(map(lambda x: x["chunk_text"], docs)),
+                                            return_documents=False,
+                                            top_k=settings.reranked_articles_max_count
+                                            )
+        final_results = []
+        for result in reranked_results:
+            doc = docs[result["corpus_id"]]     # get all the original doc fields
+            doc["rerank_relevance_score"] = result["score"]    # add rerank relevance score
+            final_results.append(doc)
+        
+        return final_results
 
 # Singleton instance
 _reranker_client = None
 
-def get_reranker_client() -> RerankerClient:
+def get_reranker_client() -> BaseRerankerClient:
     global _reranker_client
     if _reranker_client is None:
-        _reranker_client = RerankerClient()
+        if settings.reranker_model_name == "jinaai/jina-reranker-v1-tiny-en":
+            _reranker_client = JinaSmallRerankerClient()
+        else:
+            _reranker_client = JinaLargeRerankerClient()
     return _reranker_client
